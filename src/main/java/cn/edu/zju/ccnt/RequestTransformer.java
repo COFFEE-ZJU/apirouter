47f5ee395826b0b75c0d7db4b399e49a29a93bf6
Zhang Kefei <zhangkf92@gmail.com>
2015-01-12 13:35:21 +0800

    make RequestTransformer a absctrat super class, put some common code in it.

--- src/main/java/cn/edu/zju/ccnt/RequestTransformer.java
+++ src/main/java/cn/edu/zju/ccnt/RequestTransformer.java
@@ -1,28 +1,111 @@
 package cn.edu.zju.ccnt;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.apache.log4j.Logger;
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractMessageTransformer;
 
-public class RequestTransformer extends AbstractMessageTransformer {
+public abstract class RequestTransformer extends AbstractMessageTransformer {
 	Logger logger = Logger.getLogger(RequestTransformer.class);
+	
+	private MuleMessage message = null;
+	private String reqPath = null;
+	private String[] pathSplit = null;
+	private Map<String, String> query = null;
 	
 	public Object transformMessage(MuleMessage message, String outputEncoding)
 			throws TransformerException {
-		String reqPath = (String)message.getInboundProperty("http.request");
-		String[] pathSplit = reqPath.split("/");
-		if(pathSplit.length == 0 || pathSplit.length == 1){	// root path "/"
-			message.setInvocationProperty("reqHost", "www.juhe.cn");
-			message.setInvocationProperty("reqPath", "/");
-		} else {
-			message.setInvocationProperty("reqHost", "www.cc98.org");
-			message.setInvocationProperty("reqPath", reqPath);
+		logger.info(getClass().getSimpleName() + " matched");
+		this.message = message;
+		
+		init();
+		message.setInvocationProperty("reqHost", generateReqHost());
+		message.setInvocationProperty("reqPath", generateReqPath());
+		message.setInvocationProperty("httpMethod", generateHttpMethod());
+//		
+//		String reqPath = (String)message.getInboundProperty("http.request");
+//		String[] pathSplit = reqPath.split("/");
+//		if(pathSplit.length == 0 || pathSplit.length == 1){	// root path "/"
+//			message.setInvocationProperty("reqHost", "www.juhe.cn");
+//			message.setInvocationProperty("reqPath", "/");
+//		} else {
+//			message.setInvocationProperty("reqHost", "www.cc98.org");
+//			message.setInvocationProperty("reqPath", reqPath);
+//		}
+//		
+//		message.setInvocationProperty("invo", "testInvoVal");
+//		logger.info("payload: " + message.getPayload());
+		return message;
+	}
+	
+	public static String queryToString(Map<String, String> query){
+		if(query == null || query.isEmpty()) return "";
+		
+		StringBuffer buf = new StringBuffer();
+		for(Entry<String, String> entry: query.entrySet()){
+			buf.append(entry.getKey());
+			buf.append("=");
+			buf.append(entry.getValue());
+			buf.append("&");
+		}
+		return buf.substring(0, buf.length()-1);
+	}
+	
+	protected String getInboundReqPath(){
+		if(message == null) return null;
+		if(reqPath == null) reqPath = (String)message.getInboundProperty("http.request");
+		
+		return reqPath;
+	}
+	
+	protected String[] getInboundReqPathSplit(){
+		if(pathSplit == null){
+			String path = getInboundReqPath();
+			if(path == null) return null;
+			
+			pathSplit = path.split("/");
 		}
 		
-//		message.setInvocationProperty("invo", "testInvoVal");
-		logger.info("payload: " + message.getPayload());
-		return message;
+		return pathSplit;
+	}
+	
+	/**
+	 * Override it and put anything you like before we invoke generateReqHost, generateReqPath and generateHttpMethod
+	 */
+	protected void init(){}
+	
+	@SuppressWarnings("unchecked")
+	protected Map<String, String> getInboundQuery(){
+		if(message == null) return null;
+		if(query == null) query = (Map<String, String>)message.getInboundProperty("http.query.params");
+		
+		return query;
+	}
+	
+	/**
+	 * 
+	 * @return the host of the outbound api request (like "api.open.baidu.com")
+	 */
+	protected abstract String generateReqHost();
+	
+	/**
+	 * 
+	 * @return the path (along with the query params) of the outbound api request (like "/weather/q?city=Hangzhou")
+	 */
+	protected abstract String generateReqPath();
+	
+	/**
+	 * by default use whatever method the inbound request uses.
+	 * 
+	 * @return the http method type used in the api request ("GET", "POST", "PUT", etc.)
+	 */
+	protected String generateHttpMethod(){
+		if(message == null) return null;
+		
+		return message.getInboundProperty("http.method");
 	}
 
 }
--- src/main/java/cn/edu/zju/ccnt/TrainRequestTransformer.java
+++ src/main/java/cn/edu/zju/ccnt/TrainRequestTransformer.java
@@ -1,22 +1,15 @@
 package cn.edu.zju.ccnt;
 
-import org.apache.log4j.Logger;
-import org.mule.api.MuleMessage;
-import org.mule.api.transformer.TransformerException;
-import org.mule.transformer.AbstractMessageTransformer;
+public class TrainRequestTransformer extends RequestTransformer {
 
-public class TrainRequestTransformer extends AbstractMessageTransformer {
-	Logger logger = Logger.getLogger(TrainRequestTransformer.class);
-	
-	public Object transformMessage(MuleMessage message, String outputEncoding)
-			throws TransformerException {
-		logger.info("matched train regex");
-		
-		String reqPath = (String)message.getInboundProperty("http.request");
-		message.setInvocationProperty("reqHost", "apis.juhe.cn");
-		message.setInvocationProperty("reqPath", reqPath);
-		
-		return message;
+	@Override
+	protected String generateReqHost() { 
+		return "apis.juhe.cn";
+	}
+
+	@Override
+	protected String generateReqPath() {
+		return getInboundReqPath();
 	}
 
 }
