177fa89c76d60831b20aaa88429554fe3f33a68a
Zhang Kefei <zhangkf92@gmail.com>
2015-04-02 09:27:06 +0800

    temp commit

--- src/main/java/cn/edu/zju/ccnt/weather/_51wnl/WeatherRes.java
+++ src/main/java/cn/edu/zju/ccnt/weather/_51wnl/WeatherRes.java
@@ -15,3 +15,35 @@ public class WeatherRes {
 	}
 
 }
+
+@JsonAutoDetect
+class WeatherInfo {
+	private String city;
+	private String date_y;
+	private String temp1;
+	private String weather1;
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getDate_y() {
+		return date_y;
+	}
+	public void setDate_y(String date_y) {
+		this.date_y = date_y;
+	}
+	public String getTemp1() {
+		return temp1;
+	}
+	public void setTemp1(String temp1) {
+		this.temp1 = temp1;
+	}
+	public String getWeather1() {
+		return weather1;
+	}
+	public void setWeather1(String weather1) {
+		this.weather1 = weather1;
+	}
+}
